59874f5655a2a7a391f44ab87f8c18a4078b58af
ruanfernandes610 <ruanfernandes610@gmail.com>
2023-10-16 11:31:00 -0300

    first commit

---
+++ comprador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
+</head>
+<body>
+    <?php
+    // Mensagem a ser exibida
+    $mensagem = "Bem-vindo Comprador";
+
+    // Exibe a mensagem
+    echo "<h1>$mensagem</h1>";
+    ?>
+</body>
+</html>
